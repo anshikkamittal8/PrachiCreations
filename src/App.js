57468df7b94d344c06880772a527500ad377fd8d
Anshikka Mittal <anshikkamittal8@gmail.com>
2026-05-07 23:33:10 +0530

    fix: add ProductDetails import and gitignore .env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ProductDetails from './pages/ProductDetails';
 
 // Page Imports
 import Home from './pages/Home';
