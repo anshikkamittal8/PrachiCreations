711ab24441bcde22b6ad90e883b71494b9ce0ff8
Anshikka Mittal <anshikkamittal8@gmail.com>
2026-05-07 23:38:49 +0530

    fix: remove duplicate ProductDetails import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ProductDetails from './pages/ProductDetails';
 
 // Page Imports
 import Home from './pages/Home';
